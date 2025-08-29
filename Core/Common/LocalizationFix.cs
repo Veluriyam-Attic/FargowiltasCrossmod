3ba09a0d417282f8469518ca5efeb77953748083
ROH <3193934857@qq.com>
2025-08-29 09:26:41 +0800

    fix bloodmoon monolith not having localization

---
+++ Core/Common/LocalizationFix.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.Map;
+using Terraria.ModLoader;
+
+namespace FargowiltasCrossmod.Core.Common
+{
+    public class LocalizationFix : ModSystem
+    {
+        public override void Load()
+        {
+            Lang._mapLegendCache[MapHelper.TileToLookup(TileID.BloodMoonMonolith, 0)] = Lang.GetItemName(ItemID.BloodMoonMonolith);
+        }
+
+        public override void Unload()
+        {
+            Lang._mapLegendCache[MapHelper.TileToLookup(TileID.BloodMoonMonolith, 0)] = LocalizedText.Empty;
+        }
+    }
+}
